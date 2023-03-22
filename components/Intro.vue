304d6c6461368918d9517cc661f2170e53f70526
Reza Jaber <frontendrezajaber@gmail.com>
2023-03-22 16:40:14 +0100

    F: Everything for the P30 Pro

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="pt-8 px-5">
         <div class="flex items-center flex-col">
-            <p class="font-heading font-bold text-5xl">Reza Jaber</p>
-            <p class="font-heading font-bold text-[2rem] mb-1">Front-End Developer</p>
+            <p class="font-heading font-bold text-5xl text-center mb-1">Reza Jaber</p>
+            <p class="font-heading font-bold text-3xl mb-1 text-center">Front-End Developer</p>
             <img class="waving ml-2 w-14" src="../public/assets/pic/waving.png" alt="Waving-Hand">
         </div>
 
